3af749192a4645fe62db0aae4a8d85fb36212cfd
Ankit <krankit0777@gmail.com>
2025-02-22 15:49:56 +0530

    Respository initialised

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://climbing-cattle-3.clerk.accounts.dev/",
+      domain: "https://nice-walleye-68.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
